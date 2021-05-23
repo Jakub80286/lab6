e0c459be06bcafb8fa56bb0840018635270edf23
Jakub80286 <smigolek1996@gmail.com>
2021-05-23 13:24:02 +0200

    Add new files

---
+++ lab6/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 76);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(300, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 197);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(300, 56);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 259);
+            this.chart1.Name = "chart1";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "red";
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "green";
+            series12.ChartArea = "ChartArea1";
+            series12.Legend = "Legend1";
+            series12.Name = "blue";
+            this.chart1.Series.Add(series10);
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::lab6.Properties.Resources.fota;
+            this.pictureBox2.Location = new System.Drawing.Point(709, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(284, 318);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab6.Properties.Resources.fota;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 318);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 580);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ lab6/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        Image fota;
+        Bitmap bitFota;
+        int szerokosc, wysokosc;
+        public static Image obraz(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+     
+       
+        private int ciemniej(int color, int delta)
+        {
+            if (color < 127 + delta) return (127 / czyKolor(127 + delta)) * color;
+            else if (color > 127 - delta) return ((127 * color) + (255 * delta)) / czyKolor(127 + delta);
+            else return 127;
+        }
+        public int czyKolor(int value)
+        {
+            if (value >= 255) return 254;
+            if (value <= 0) return 1;
+            else return value;
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            fota = pictureBox1.Image;
+            fota = obraz(fota, new Size(pictureBox1.Size.Width, pictureBox1.Size.Height));
+            bitFota = new Bitmap(fota);
+            szerokosc = bitFota.Width;
+            wysokosc = bitFota.Height;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Bitmap ustal = new Bitmap(fota);
+            for (int y = 0; y < wysokosc; y++)
+            {
+                for (int x = 0; x < szerokosc; x++)
+                {
+                    Color tukanPixel = bitFota.GetPixel(x, y);
+
+                    int a = tukanPixel.A;
+                    int r = czyKolor((127 / czyKolor(127 - trackBar1.Value)) * (tukanPixel.R - trackBar1.Value));
+                    int g = czyKolor((127 / czyKolor(127 - trackBar1.Value)) * (tukanPixel.G - trackBar1.Value));
+                    int b = czyKolor((127 / czyKolor(127 - trackBar1.Value)) * (tukanPixel.B - trackBar1.Value));
+                    ustal.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+            kolHis(ustal);
+            pictureBox2.Image = ustal;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Bitmap ustal = new Bitmap(fota);
+            for (int y = 0; y < wysokosc; y++)
+            {
+                for (int x = 0; x < szerokosc; x++)
+                {
+                    Color tukanPixel = bitFota.GetPixel(x, y);
+
+                    int a = tukanPixel.A;
+                    int r = ciemniej(tukanPixel.R, trackBar2.Value);
+                    int g = ciemniej(tukanPixel.G, trackBar2.Value);
+                    int b = ciemniej(tukanPixel.B, trackBar2.Value);
+                    ustal.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+            kolHis(ustal);
+            pictureBox2.Image = ustal;
+        }
+
+        private void kolHis(Bitmap temp)
+        {
+            int[] czerw = new int[256];
+            int[] ziel = new int[256];
+            int[] nieb = new int[256];
+            for (int x = 0; x < szerokosc; x++)
+            {
+                for (int y = 0; y < wysokosc; y++)
+                {
+                    Color pixel = temp.GetPixel(x, y);
+                    czerw[pixel.R]++;
+                    ziel[pixel.G]++;
+                    nieb[pixel.B]++;
+                }
+            }
+
+            
+            chart1.Series["red"].Points.Clear();
+            chart1.Series["green"].Points.Clear();
+            chart1.Series["blue"].Points.Clear();
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series["red"].Points.AddXY(i, czerw[i]);
+                chart1.Series["green"].Points.AddXY(i, ziel[i]);
+                chart1.Series["blue"].Points.AddXY(i, nieb[i]);
+            }
+            chart1.Invalidate();
+        }
+    }
+
+}
